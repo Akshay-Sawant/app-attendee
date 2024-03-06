9bbf90f888eb270f319463354e0d2a4cc7d33d98
Akshay Sawant <akshay.sawant@pebstone.com>
2024-03-06 20:10:02 +0530

    Changes made in App.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Aabid
+          Learn React Team Teno
         </a>
       </header>
     </div>
